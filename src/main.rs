6b498ae23b02eab15202ccff9b0dcb77e8ca35a0
Jacob Coughenour <me@jacobcoughenour.com>
2024-01-11 21:27:44 -0500

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,7 @@ fn handle_request(stream: [u8; 4096]) -> String {
 
     if !requested_path.starts_with("/") {
         return respond(Some(200), None, None);
-    } 
+    }
 
     let requested_path_split: Vec<&str> = requested_path.split("/").collect();
     
