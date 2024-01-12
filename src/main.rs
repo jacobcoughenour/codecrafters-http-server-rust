1c4d82e46b359d564b3a07758f9489bcc49e7b63
Jacob Coughenour <me@jacobcoughenour.com>
2024-01-11 20:31:12 -0500

    third stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::Write;
+use std::io::Read;
 use std::net::TcpListener;
 
 fn main() {
@@ -9,19 +10,48 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
 
-                let status = "200 OK";
-                let headers = "";
-
-                _stream
-                    .write(format!("HTTP/1.1 {status}\r\n{headers}\r\n").as_bytes())
-                    .unwrap();
+                let mut buffer = [0u8; 4096];
+                stream.read(&mut buffer).unwrap();
+                let response = handle_request(buffer);
+                stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_request(stream: [u8; 4096]) -> String {
+
+    let mut status = "";
+    let mut headers = "";
+
+    // read the request
+    let request_str = String::from_utf8_lossy(&stream);
+    let request_lines: Vec<&str> = request_str.split("\r\n").collect();
+
+    if (request_lines.len() == 0) {
+        status = "400 Bad Request";
+    } else {
+        let first_line = request_lines[0];
+        let first_line_split: Vec<&str> = first_line.split(" ").collect();
+
+        let requested_path = first_line_split[1];
+        println!("requested path: {}", requested_path);
+    
+        match requested_path {
+            "/" => {
+               status = "200 OK";
+            }
+            _ => {
+                status = "404 Not Found";
+            }
+        }
+    }
+
+    return format!("HTTP/1.1 {status}\r\n{headers}\r\n");
 }
